f4de8b9cf4b539c58bd845f6c93e14c4157c62c6
Nicholas Restifo <nsrestifo@hotmail.com>
2017-04-13 21:16:24 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :arguments
+  resources :design_axis
+  resources :mature_investments
+  resources :models
+  resources :parameters
+  resources :simulations
+  resources :stock_days
+  resources :stock_histories
+  resources :versions
 end
