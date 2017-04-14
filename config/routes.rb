7352c5fb405f0aa14f0755050e4608b8acf508c4
Nicholas Restifo <nsrestifo@hotmail.com>
2017-04-13 22:15:45 -0400

    disabled all routes except stock histories

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :arguments
-  resources :design_axis
-  resources :mature_investments
-  resources :models
-  resources :parameters
-  resources :simulations
-  resources :stock_days
+  # resources :arguments
+  # resources :design_axis
+  # resources :mature_investments
+  # resources :models
+  # resources :parameters
+  # resources :simulations
+  # resources :stock_days
   resources :stock_histories
-  resources :versions
+  # resources :versions
 end
