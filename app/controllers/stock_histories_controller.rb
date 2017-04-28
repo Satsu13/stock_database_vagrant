dda7530c50fbaaa146bcbb59dd6f3bbd62341ad2
Nicholas Restifo <nsrestifo@hotmail.com>
2017-04-27 21:59:43 -0400

    updated controller

--- app/controllers/stock_histories_controller.rb
+++ app/controllers/stock_histories_controller.rb
@@ -1,6 +1,7 @@
 class StockHistoriesController < ApplicationController
   def show
-      @stock_history = StockHistories.find(params[:id])
-      render :json => @stock_history
+    puts "SHOWING"
+    stock_history = StockHistories.find(params[:id])
+    render :json => stock_history
   end
 end
